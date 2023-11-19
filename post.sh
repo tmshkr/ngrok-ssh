907969d7eb43de6ce9d5c79dbce1d192d397765d
Tim Shaker <tim@tmshkr.com>
2023-11-19 12:30:24 -0800

    cleanup

--- post.sh
+++ post.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
+if [ $GITHUB_ACTIONS == false ]; then
+  HOME="$PWD/dev"
+  USER="dev"
+fi
+
+ssh_dir="$HOME/.ssh"
+ngrok_dir="$HOME/.ngrok"
+
 
 # Wait until there are no users logged in
 while ss -tnp | grep sshd | grep $INPUT_SSH_PORT ; do
@@ -6,4 +14,13 @@ while ss -tnp | grep sshd | grep $INPUT_SSH_PORT ; do
   sleep 5
 done
 
-echo "All users logged out. Exiting..."
+
+echo "All users logged out. Cleaning up..."
+echo "killall ngrok tmux"
+killall ngrok tmux || true
+echo "kill sshd process"
+kill $(cat $ssh_dir/sshd.pid) || true
+echo "Deleting $ssh_dir"
+rm -rf $ssh_dir || true
+echo "Deleting $ngrok_dir"
+rm -rf $ngrok_dir || true
