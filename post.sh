a1e002ee4a07cf16eb8c6109546d3c543c00eb8b
Tim Shaker <tim@tmshkr.com>
2023-11-19 15:22:04 -0800

    add INPUT_WAIT_FOR_CONNECTION

--- post.sh
+++ post.sh
@@ -8,6 +8,14 @@ ssh_dir="$HOME/.ssh"
 ngrok_dir="$HOME/.ngrok"
 
 
+if [ "$INPUT_WAIT_FOR_CONNECTION" == true ] && [ ! -f "$ssh_dir/connections" ]; then
+  while [ ! -f "$ssh_dir/connections" ]; do
+    echo "Waiting for SSH user to login..."
+    sleep 5
+  done
+fi
+
+
 # Wait until there are no users logged in
 while ss -tnp | grep sshd | grep $INPUT_SSH_PORT ; do
   echo "Waiting for all users to log out..."
@@ -16,9 +24,8 @@ done
 
 
 echo "All users logged out. Cleaning up..."
-echo "killall ngrok tmux"
-killall ngrok tmux || true
-echo "kill sshd process"
+echo "Terminating processes..."
+killall ngrok || true
 kill $(cat $ssh_dir/sshd.pid) || true
 echo "Deleting $ssh_dir"
 rm -rf $ssh_dir || true
