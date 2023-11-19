39bf7021cf3e863fa9b7f1f5730d797b31db4087
Tim Shaker <tim@tmshkr.com>
2023-11-18 22:14:04 -0800

    get actionPath from env

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const { execSync } = require("child_process");
-const actionPath = `/home/runner/work/_actions/tmshkr/ngrok-ssh/dev`;
+
+const {
+  GITHUB_ACTION,
+  GITHUB_ACTION_REF,
+  GITHUB_ACTION_REPOSITORY,
+  GITHUB_WORKSPACE,
+} = process.env;
+
+const actionPath =
+  GITHUB_ACTION === "__self"
+    ? GITHUB_WORKSPACE
+    : `/home/runner/work/_actions/${GITHUB_ACTION_REPOSITORY}/${GITHUB_ACTION_REF}`;
+
 try {
   execSync(`ACTION_PATH=${actionPath} ${actionPath}/main.sh`, {
     stdio: "inherit",
