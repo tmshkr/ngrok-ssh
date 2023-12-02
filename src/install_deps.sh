f281cef371006df2c72d46eb40ae52011c26ccee
Tim Shaker <tim@tmshkr.com>
2023-12-02 14:09:37 -0800

    use apt

--- src/install_deps.sh
+++ src/install_deps.sh
@@ -1,39 +1,48 @@
 #!/bin/bash -e
 
+apt_deps=()
+
+if ! command -v "curl" >/dev/null 2>&1; then
+  echo "Installing curl..."
+  apt_deps+=("curl")
+fi
+
 if ! command -v "envsubst" >/dev/null 2>&1; then
-  echo "Installing envsubst..."
-  wget -q https://github.com/a8m/envsubst/releases/download/v1.4.2/envsubst-Linux-x86_64
-  chmod +x envsubst-Linux-x86_64
-  mv envsubst-Linux-x86_64 /usr/local/bin/envsubst
+  echo "Installing gettext-base..."
+  apt_deps+=("gettext-base")
 fi
 
 if ! command -v "jq" >/dev/null 2>&1; then
   echo "Installing jq..."
-  wget -q https://github.com/jqlang/jq/releases/download/jq-1.7/jq-linux-amd64
-  chmod +x jq-linux-amd64
-  mv jq-linux-amd64 /usr/local/bin/jq
-fi
-
-if ! command -v "ngrok" >/dev/null 2>&1; then
-  echo "Installing ngrok..."
-  wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-  tar -xzf ngrok-v3-stable-linux-amd64.tgz
-  chmod +x ngrok
-  mv ngrok /usr/local/bin/ngrok
-  rm ngrok-v3-stable-linux-amd64.tgz
+  apt_deps+=("jq")
 fi
 
 if ! command -v "sshd" >/dev/null 2>&1; then
   echo "Installing sshd..."
-  su -c "apt-get update && apt-get install openssh-server -y && mkdir /run/sshd"
+  apt_deps+=("openssh-server")
+  su -c "mkdir /run/sshd"
 fi
 
 if ! command -v "ss" >/dev/null 2>&1; then
   echo "Installing iproute2..."
-  su -c "apt-get update && apt-get install iproute2 -y"
+  apt_deps+=("iproute2")
 fi
 
 if ! command -v "tmux" >/dev/null 2>&1; then
   echo "Installing tmux..."
-  su -c "apt-get update && apt-get install tmux -y"
+  apt_deps+=("tmux")
+fi
+
+if [ ${#apt_deps[@]} -gt 0 ]; then
+  echo "Installing ${#apt_deps[@]} dependencies..."
+  su -c "apt-get update && apt-get install ${apt_deps[*]} -y"
+fi
+
+if ! command -v "ngrok" >/dev/null 2>&1; then
+  echo "Installing ngrok..."
+  curl -O https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+  tar -xzf ngrok-v3-stable-linux-amd64.tgz
+  chmod +x ngrok
+  mv ngrok /usr/local/bin/ngrok
+  rm ngrok-v3-stable-linux-amd64.tgz
 fi
