f92377699546ce4f69b427b56222b1a7269fc44e
Tim Shaker <tim@tmshkr.com>
2023-12-04 10:42:03 -0800

    sudo

--- test/setup-ssh-client.sh
+++ test/setup-ssh-client.sh
@@ -12,8 +12,8 @@ Host $SSH_HOSTNAME
   UserKnownHostsFile $HOME/.ssh/known_hosts
 "
 
-mkdir -m 600 -p $HOME/.ssh
-chmod 600 $HOME/.ssh/*
+sudo mkdir -m 600 -p $HOME/.ssh
+sudo chmod 600 $HOME/.ssh/*
 echo "$SSH_CLIENT_PUBLIC_KEY" >$HOME/.ssh/id_rsa.pub
 echo "$SSH_CLIENT_PRIVATE_KEY" >$HOME/.ssh/id_rsa
 echo "$ssh_config" >$HOME/.ssh/config
